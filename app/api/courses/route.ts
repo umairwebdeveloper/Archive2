7a8193908cf06f5a5fef36f49a3a154151432dbb
umairwebdeveloper <umairashraf5252@gmail.com>
2024-05-26 21:45:37 +0500

    fixed

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -42,7 +42,7 @@ export async function GET() {
       },
     });
 
-    return NextResponse.json(courses);
+    return NextResponse.json({'courses': courses});
   } catch (error) {
     console.log("[COURSES]", error);
     return new NextResponse("Internal Error", { status: 500 });
